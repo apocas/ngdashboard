dd504c8886b97f3be7f98221458976d2f908ac48
Pedro Dias <petermdias@gmail.com>
2016-12-25 18:00:01 +0000

    Dashboard initial commit

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-var Cache = new require('./cache');
+var Cache = new require('./lib/cache');
+var Dashboard = new require('./lib/dashboard');
 
 var path = '/dev/shm/nginxfastcgi/';
 
 var c = new Cache(path);
 c.start();
+
+var d = new Dashboard(8888, c);
+d.start();
